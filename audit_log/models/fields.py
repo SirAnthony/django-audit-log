bf546c05973f06aa80df5c67f4e99fa07a00f64a
Bill Maloney <billm@commonplaces.com>
2013-04-11 03:38:06 -0400

    Use settings.AUTH_USER_MODEL for LastUserField in Django 1.5+

--- audit_log/models/fields.py
+++ audit_log/models/fields.py
@@ -1,6 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from audit_log import registration
+
+try:
+    # Django 1.5+
+    user_model = settings.AUTH_USER_MODEL
+except AttributeError:
+    # Django < 1.5
+    from django.contrib.auth.models import User
+    user_model = User
+
 
 class LastUserField(models.ForeignKey):
     """
@@ -8,7 +17,7 @@ class LastUserField(models.ForeignKey):
     of a model. None will be the value for AnonymousUser.
     """
     
-    def __init__(self, to = User, null = True,  **kwargs):
+    def __init__(self, to = user_model, null = True,  **kwargs):
         super(LastUserField, self).__init__(to = to, null = null, **kwargs)
     
     def contribute_to_class(self, cls, name):
